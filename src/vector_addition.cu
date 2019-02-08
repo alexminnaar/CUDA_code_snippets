1ad4388f65eca87e6d31541026d3821bc37d9ff0
Alex Minnaar <minnaaralex@gmail.com>
2019-02-07 18:25:08 -0800

    updated vector addition and added error handling

--- src/vector_addition.cu
+++ src/vector_addition.cu
@@ -2,61 +2,70 @@
 #include <stdlib.h>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include "common.h"
 
-#define N 10
+#define N   (32 * 1024)
 
-__global__
-void add(int *a, int *b,int *c){
-	//get unique thread id
-	int tid = blockIdx.x;
-
-	if (tid < N){
-		c[tid] = a[tid]+b[tid];
-	}
+__global__ void add( int *a, int *b, int *c ) {
+    int tid = blockIdx.x;
+    while (tid < N) {
+        c[tid] = a[tid] + b[tid];
+        tid += gridDim.x;
+    }
 }
 
+int main( void ) {
+    int *a, *b, *c;
+    int *dev_a, *dev_b, *dev_c;
 
-int main()
-{
+    // allocate the memory on the CPU
+    a = (int*)malloc( N * sizeof(int) );
+    b = (int*)malloc( N * sizeof(int) );
+    c = (int*)malloc( N * sizeof(int) );
 
-	int a[N], b[N], c[N];
-	int *dev_a, *dev_b, *dev_c;
+    // allocate the memory on the GPU
+    HANDLE_ERROR( cudaMalloc( (void**)&dev_a, N * sizeof(int) ) );
+    HANDLE_ERROR( cudaMalloc( (void**)&dev_b, N * sizeof(int) ) );
+    HANDLE_ERROR( cudaMalloc( (void**)&dev_c, N * sizeof(int) ) );
 
-	//allocate memory on device for the three arrays
-	cudaMalloc((void**)&dev_a,N*sizeof(int));
-	cudaMalloc((void**)&dev_b,N*sizeof(int));
-	cudaMalloc((void**)&dev_c,N*sizeof(int));
+    // fill the arrays 'a' and 'b' on the CPU
+    for (int i=0; i<N; i++) {
+        a[i] = i;
+        b[i] = 2 * i;
+    }
 
-	//fill arrays on host
-	for(int i = 0 ; i<N;i++){
-		a[i]= -i;
-		b[i]= i*i;
-	}
+    // copy the arrays 'a' and 'b' to the GPU
+    HANDLE_ERROR( cudaMemcpy( dev_a, a, N * sizeof(int),
+                              cudaMemcpyHostToDevice ) );
+    HANDLE_ERROR( cudaMemcpy( dev_b, b, N * sizeof(int),
+                              cudaMemcpyHostToDevice ) );
 
+    add<<<128,1>>>( dev_a, dev_b, dev_c );
 
-	//copy arrays to GPU
-	cudaMemcpy(dev_a,a,N*sizeof(int),cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_b,b,N*sizeof(int),cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_c,c,N*sizeof(int),cudaMemcpyHostToDevice);
+    // copy the array 'c' back from the GPU to the CPU
+    HANDLE_ERROR( cudaMemcpy( c, dev_c, N * sizeof(int),
+                              cudaMemcpyDeviceToHost ) );
 
+    // verify that the GPU did the work we requested
+    bool success = true;
+    for (int i=0; i<N; i++) {
+        if ((a[i] + b[i]) != c[i]) {
+            printf( "Error:  %d + %d != %d\n", a[i], b[i], c[i] );
+            success = false;
+        }
+    }
+    if (success)    printf( "We did it!\n" );
 
-	//call cuda kernel
-	add<<<N,1>>>(dev_a,dev_b,dev_c);
+    // free the memory we allocated on the GPU
+    HANDLE_ERROR( cudaFree( dev_a ) );
+    HANDLE_ERROR( cudaFree( dev_b ) );
+    HANDLE_ERROR( cudaFree( dev_c ) );
 
-	//copy c array back to host
-	cudaMemcpy(c,dev_c, N*sizeof(int), cudaMemcpyDeviceToHost);
+    // free the memory we allocated on the CPU
+    free( a );
+    free( b );
+    free( c );
 
-
-	//print results
-	for(int i=0; i<N;i++){
-		printf("%d + %d = %d\n",a[i],b[i],c[i]);
-	}
-
-	//free allocated memory on device
-	cudaFree(dev_a);
-	cudaFree(dev_a);
-	cudaFree(dev_a);
-
-	printf("done");
-	return EXIT_SUCCESS;
+    return 0;
 }
+
